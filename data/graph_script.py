e99d579154c79977992185e24ec9cc2fb8eab3ef
Ilem Leisher <ilemleisher@gmail.com>
2025-11-20 21:37:33 -0600

    Add graph creation script for merger tree data

---
+++ data/graph_script.py
@@ -0,0 +1,223 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py, os, optuna, torch, pickle
+from scipy.spatial import KDTree
+from torch_geometric.data import Data
+from torch_geometric.loader import DataLoader
+import torch_geometric as torchg
+import torch_scatter as torchs
+import sys
+
+def get_params(file):
+    """
+    This function reads in the simulation parameters for all DREAMS sims and converts them into physical units
+    
+    Inputs
+     - file - absolute or relative path to the file containing all simulation parameters
+     
+    Returns
+     - params - an Nx4 ndarray with the following parameters
+                index 0 - WDM particle mass [keV]
+                index 1 - SN1 (ew) wind energy
+                index 2 - SN2 (kw) wind velocity
+                index 3 - AGN1 (BHFF) black hole feedback factor
+    """
+    sample = np.loadtxt(file)
+    
+    fiducial = np.array([1,3.6,7.4,.1]) #fiducial TNG values
+    params = sample * fiducial
+    params[:,0] = 1/params[:,0]
+    
+    return params
+
+def norm_params(params):
+    """
+    This function normalizes the four simulation parameters (WDN, SN1, SN2, AGN).
+    
+    Inputs
+     - params - an Nx4 array of simulation parameters
+    
+    Results
+     - nparams - same as the input but now normalized and linearly sampled between 0 and 1
+    """
+    nparams = params / np.array([1, 3.6, 7.4, .1])
+    nparams[:,0] = 1/nparams[:,0]
+    
+    nparams[:,1:] = np.log10(nparams[:,1:])
+
+    minimum = np.array([1/30, np.log10(0.25), np.log10(0.5), np.log10(0.25)])
+    maximum = np.array([1/1.8, np.log10(4.0), np.log10(2.0), np.log10(4.0)])
+
+    nparams = (nparams - minimum)/(maximum - minimum)
+
+    return nparams
+
+def denormalize(true, pred, err, type="WDM"):
+    """
+    This function denormalizes the results from the trained GNN which predicts the parameter values
+    
+    Inputs
+     - true - the correct parameter values from the simulation
+     - pred - the predicted parameter values from the GNN
+     - err  - the predicted parameter values from the GNN
+     
+    Returns
+     - ntrue - the denormalized true array
+     - npred - the denormalized pred array
+     - nerr  - the denormalized err array
+    """
+    if type == "WDM":
+        mi = 1/30
+        ma = 1/1.8
+        ntrue = mi + true*(ma - mi)
+        npred = mi + pred*(ma - mi)
+        nerr = err*(ma-mi)
+
+    elif type == "SN1":
+        mi = np.log10(0.25)
+        ma = np.log10(4.0)
+        ntrue = mi + true*(ma - mi)
+        npred = mi + pred*(ma - mi)
+        nerr = err*(ma-mi)
+        
+    elif type == "SN2":
+        mi = np.log10(0.5)
+        ma = np.log10(2.0)
+        ntrue = mi + true*(ma - mi)
+        npred = mi + pred*(ma - mi)
+        nerr = err*(ma-mi)
+        
+    elif type == "AGN":
+        mi = np.log10(0.25)
+        ma = np.log10(4.0)
+        ntrue = mi + true*(ma - mi)
+        npred = mi + pred*(ma - mi)
+        nerr = err*(ma-mi)
+    
+    return ntrue, npred, nerr
+
+
+## Creating dataset from loaded data
+def create_dataset(cat, params):
+    """
+    This function turns DREAMS catalogs into pytorch_geometric Data objects which contain the node, edge, and graph data and the correct parameter values for each graph.
+    These Data objects can then be used to create pytorch_geometric DataLoader objects that will load the data during training.
+
+    Inputs
+     - cat - the raw merger tree dataset for a single simulation box
+     - params - an array of correct parameter values that will be used during training
+
+    Results
+     - graph - a pytorch_geometric Data object containing the node, edge, graph, and parameter values
+    """
+    
+    # Required info for creating graphs
+    snap = cat['SnapNum']
+    subid = cat['SubhaloID']
+    desid = cat['DescendantID']
+
+    nodes = []
+
+    for arg in sys.argv[4:]:
+        if type(cat[arg]) != np.ndarray:
+            nodes.append(cat[arg])
+        else:
+            for i in len(cat[arg]):
+              nodes.append(cat[arg][i])
+
+    revisit this!! ^^^^
+    
+    nodes_tuple = tuple(nodes)
+
+    
+    data = np.column_stack(nodes_tuple) #Node features associated with each node in the graph
+    x = torch.tensor(data, dtype=torch.float32)
+        
+    ngal = len(snap)
+    u = np.log10(ngal).reshape(1,1)
+    u = torch.tensor(u, dtype=torch.float32)
+
+    y = np.reshape(params, (1,params.shape[0]))
+    y = torch.tensor(y, dtype=torch.float32)
+
+    edge_index = get_edges(subid,desid)
+    edge_index = torch.tensor(edge_index, dtype=torch.long)
+
+    edge_attr=np.ones((edge_index.shape[1],3))
+    edge_attr = torch.tensor(edge_attr, dtype=torch.float32)
+    
+    graph = Data(x=x, y=y, u=u, edge_index=edge_index,edge_attr=edge_attr)
+
+    return graph
+
+
+def get_edges(subid,desid):
+
+    """
+    A function taken from Cosmographnet to create edges between a pair of nodes if their distance is less than the linking radius
+
+    Inputs
+     - subid - list of subhalo ids from a single merger tree
+     - desid - list of subhalo descendant ids from a single merger tree
+
+    Returns
+     - edge_index - Nx2 array of indecies which point to a pair nodes connected by an edge
+    """
+
+    # Draws edges directed backwards in time
+    start_edges = []
+    end_edges = []
+    for i in range(len(subid)):
+        for j in range(len(desid)):
+            if subid[i] == desid[j]:
+                start_edges.append([i])
+                end_edges.append([j])
+    
+    edges = []
+    edges.append(start_edges)
+    edges.append(end_edges)
+    
+    edge_index = np.array(edges)
+
+    #reformat for pytorch
+    edge_index = edge_index.reshape((2,-1)).astype(int)
+
+    edge_index = edge_index.astype(int)
+
+    return edge_index
+
+if __name__ == "__main__":
+    # Load in data
+    with open(sys.argv[1], 'rb') as f:
+        catalogs = pickle.load(f)
+    
+    # path to parameter file
+    param_path = sys.argv[2]
+    params = []
+    boxes = range(len(catalogs)))
+    for box in boxes:
+        try:
+            param = get_params(param_path)[box]
+            params.append(param)
+        except:
+            print(box)
+    
+    params = np.array(params)
+    nparams = norm_params(params)
+  
+    if sys.argv[3] == 'WDM':
+        params = nparams[:,0:1]
+    elif sys.argv[3] == 'SN1':
+        params = nparams[:,1:2]
+    elif sys.argv[3] == 'SN2':
+        params = nparams[:,2:3]
+    elif sys.argv[3] == 'AGN':
+        params = nparams[:,3:4]
+    
+    # Create dataset
+    dataset = []
+    for i in range(len(catalogs)):
+        dataset.append(create_dataset(catalogs[i], params[i]))
+
+    with open("merger_tree_graph_data", 'wb') as f:
+        pickle.dump(dataset, f)
